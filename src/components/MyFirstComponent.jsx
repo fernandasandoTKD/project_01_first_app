6e748a48f7781b63393a37e919d96ff4aa31e9d0
Fernanda Sandoval <luisa.sandoval@commity.co>
2024-06-27 07:59:14 -0500

    PR01: My first component

---
+++ src/components/MyFirstComponent.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const MyFirstComponent = () => {
+  return (
+    <>
+    <h1>My First Component</h1>
+    <p>Hello, world, I'am Fernada Díaz</p>
+    </>
+  )
+}
